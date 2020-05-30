f8cefd1e764933ffe29686794a2f9723fd8c9cf9
tedzregor <tedzzzregor@gmail.com>
2020-05-30 18:40:16 +0800

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+    {
+        id:1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id:2,
+        name: 'Teddy Du',
+        username: 'ted',
+        email: 'tedzzzregor@gmail.com'
+    },
+    {
+        id:3,
+        name: 'Sha T',
+        username: 'JS',
+        email: 'shaT@gmail.com'
+    },
+    {
+        id:4,
+        name: 'Jhoven Chu',
+        username: 'JC',
+        email: 'JC@gmail.com'
+    }
+]
